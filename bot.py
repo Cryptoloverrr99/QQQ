160af04eb46c6781f550e83fa106754af35378f0
Cryptoloverrr99 <digitalmarketingpro22@gmail.com>
2025-02-03 20:09:24 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -129,5 +129,8 @@ def run_check(self):
 if __name__ == '__main__':
     analyzer = TokenAnalyzer()
     scheduler = BlockingScheduler(timezone="UTC")
-    scheduler.add_job(analyzer.run_check, 'interval', seconds=180)
-    scheduler.start()
+    scheduler.add_job(
+    analyzer.format_alert,  # Nom corrigé
+    'interval', 
+    seconds=180
+    )
